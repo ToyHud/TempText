254745261ba86305d39928bf9c3f8d204763d029
ToyiaRenae <latoyia.short@gmail.com>
2022-03-05 13:21:16 -0500

    merge viewmodel

--- app/src/main/java/com/example/android/temptext/MainActivity.kt
+++ app/src/main/java/com/example/android/temptext/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.android.temptext
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import com.example.android.temptext.viewmodel.TempTextViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: TempTextViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
