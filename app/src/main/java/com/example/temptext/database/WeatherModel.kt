be8527e023bb0db10c1d2e6e5b951937dd7163db
SpinningLens <86024223+SpinningLens@users.noreply.github.com>
2022-03-17 19:40:22 -0500

    update database

--- app/src/main/java/com/example/temptext/database/WeatherModel.kt
+++ app/src/main/java/com/example/temptext/database/WeatherModel.kt
@@ -13,18 +13,15 @@ data class WeatherModel(
     @ColumnInfo(name = "current") val currentWeather: String,
     @ColumnInfo(name = "temp_c") val celsius : Float?,
     @ColumnInfo(name =  "name") val city: String?,
-    @ColumnInfo(name = "condition") val currentWeatherCondition: WeatherConditions?,
+    //@ColumnInfo(name = "condition") val currentWeatherCondition: WeatherConditions?,
     @ColumnInfo(name = "precip_in")val precipitation: Float?,
     @ColumnInfo(name = "humidity") val humidity:Double?,
-    @ColumnInfo(name = "air_quality")val aqi: WeatherConditions?,
+    //@ColumnInfo(name = "air_quality")val aqi: WeatherConditions?,
     @ColumnInfo(name = "wind-mph") val wind: Float?,
     @ColumnInfo(name = "text") val currentCondition:String?,
     @ColumnInfo(name = "co")val ozone : Float?
 )
 
-class WeatherConditions(
-    @Json(name= "text") val currentCondition: String?,
-    @Json (name = "air_quality") val aqi: String?
 
 
 
@@ -32,4 +29,5 @@ class WeatherConditions(
 
 
 
-)
+
+
