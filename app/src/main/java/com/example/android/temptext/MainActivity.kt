df4c0125fa81d116c639fea526026a91e9f02eac
Ayana Bando <ayanaflax@yahoo.com>
2022-03-26 16:53:50 -0400

    add notification builder icons and description

--- app/src/main/java/com/example/android/temptext/MainActivity.kt
+++ app/src/main/java/com/example/android/temptext/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android.temptext
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
+import androidx.core.app.NotificationCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.android.temptext.databinding.ActivityMainBinding
@@ -10,6 +13,7 @@ import com.example.android.temptext.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    private val CHANNEL_ID = "25"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
