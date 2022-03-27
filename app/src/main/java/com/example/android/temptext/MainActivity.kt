6282d6a2212d3047727663e714c0a4ab96addd5f
Ayana Bando <ayanaflax@yahoo.com>
2022-03-27 19:06:11 -0400

    update notification function

--- app/src/main/java/com/example/android/temptext/MainActivity.kt
+++ app/src/main/java/com/example/android/temptext/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.android.temptext
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.android.temptext.databinding.ActivityMainBinding
@@ -23,18 +27,33 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_graph) as NavHostFragment
         navController = navHostFragment.navController
+        createNotificationChannel()
+        binding.button.setOnClickListener { notification() }
     }
-   override fun onSupportNavigateUp(): Boolean {
-       return navController.navigateUp() || super.onSupportNavigateUp()
-   }
-    fun notification (){
-       val textTitle = "alert"
-       val textContent = "weather alerts"
-           var builder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
-       .setSmallIcon(R.drawable.notification_icon)
-       .setContentTitle(textTitle)
-       .setContentText(textContent)
-       .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    fun notification() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent= PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val textTitle = "Temp Text Alert"
+        val textContent = "Current temp is 70. Stay cool!"
+        val notificationId = 1
+        var builder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.notification_icon)
+            .setContentTitle(textTitle)
+            .setContentText(textContent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(notificationId, builder.build())
+        }
     }
 
     private fun createNotificationChannel() {
@@ -44,8 +63,11 @@ class MainActivity : AppCompatActivity() {
             val name = getString(R.string.channel_name)
             val descriptionText = getString(R.string.channel_description)
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance)
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance).apply {
+                description= descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
-
 }
